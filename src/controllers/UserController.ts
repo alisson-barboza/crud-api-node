9f7f0776fb83fd85cc77970a944d00fcc5456b81
Alisson <alisson.barbozalima@gmail.com>
2019-10-13 11:15:25 -0300

    Add validation to missing request body parameters

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -5,15 +5,24 @@ import { User } from '../models/User'
 class UserController {
   public create (req: Request, res: Response) : Response {
     const body = req.body
+    if(!body['name'] || !body['email'] || !body['gender']){
+      return res.status(400).json({error: 'Attribute undefined'})      
+    }
     const user = new User(body.name, body.email, body.gender)
     return res.json(userService.createUser(user))
   }
 
   public add (req:Request, res:Response): Response {
-    return res.json(userService.addToLine(req.body.id))
+    if(!req.body['id']){
+      return res.status(400).json({error: 'Id undefined'})      
+    }          
+    return res.json(userService.addToLine(req.body.id))     
   }
 
   public findPosition (req:Request, res:Response): Response {
+    if(!req.body['email']){
+      return res.status(400).json({error: 'Email undefined'})      
+    }
     return res.json(userService.findPosition(req.body.email))
   }
 
@@ -22,6 +31,9 @@ class UserController {
   }
 
   public filterLine (req:Request, res:Response): Response {
+    if(!req.body['gender']){
+      return res.status(400).json({error: 'Gender undefined'})      
+    }
     return res.json(userService.filterLine(req.body.gender))
   }
 
