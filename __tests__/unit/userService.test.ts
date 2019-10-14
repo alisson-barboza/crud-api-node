fc47b61112d5cd216e678368221958279115ee63
Alisson <alisson.barbozalima@gmail.com>
2019-10-14 20:04:35 -0300

    Update first unit test (working now)

--- __tests__/unit/userService.test.ts
+++ __tests__/unit/userService.test.ts
@@ -7,5 +7,10 @@ it('should create an user and return it with an id', () =>{
     const user = new User('alisson', 'email@gmail.com', 'male');
     const userCreated = UserService.createUser(user)
 
-    expect(userCreated.id).toBeNull()
+    expect(userCreated.name).toBe(user.name)
+    expect(userCreated.email).toBe(user.email)
+    expect(userCreated.gender).toBe(user.gender)
+    expect(userCreated.id).not.toBeNull()
 })
+
+
