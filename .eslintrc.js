0470eaa8fd99c925460a334b07237eff0ce77cb6
Alisson <alisson.barbozalima@gmail.com>
2019-10-15 00:08:30 -0300

    Add lint configurations

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'standard',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style'
+  ],
+  plugins: ['@typescript-eslint'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+  }
+}
